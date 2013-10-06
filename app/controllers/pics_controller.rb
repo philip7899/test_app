2377dbe63128a5a99b843c1fc85910cc8aa437df
Philip Stitzer <pghrpg@gmail.com>
2013-10-05 20:49:40 -0400

    Simple form and new action in pics controller

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,4 +2,13 @@ class PicsController < ApplicationController
 	def index
 		@pics = Pic.all
 	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.create(params[:pic])
+		redirect_to pics_path
+	end
 end
