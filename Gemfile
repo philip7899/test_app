4ad0fae3c40a200431653a76c4ff62b8c2269d2d
Philip Stitzer <pghrpg@gmail.com>
2013-10-06 15:18:58 -0400

    Allow users to upload images

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jquery-rails'
 gem "twitter-bootstrap-rails"
 
 gem "simple_form"
+gem 'carrierwave'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,5 +1,6 @@
 class Pic < ActiveRecord::Base
-   attr_accessible :hour, :emotion, :learned
+   attr_accessible :hour, :emotion, :learned, :image
    has_many :votes
+   mount_uploader :image, ImageUploader
 
 end
